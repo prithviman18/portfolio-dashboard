f051a9031309123dcd7d1a3e0c3aacc072c6b124
prithviman18 <prithvimanojoffc18@gmail.com>
2026-01-06 20:38:33 +0530

    Home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import PortfolioTable from "@/components/PortfolioTable";
+import { ChartBarIcon } from "@heroicons/react/16/solid";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -51,14 +52,34 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50 p-4 md:p-8">
-      <header className="mb-8">
-        <h1 className="text-3xl font-bold text-gray-900">
-          Portfolio Dashboard
-        </h1>
-        <p className="text-gray-700 mt-2 mb-2 font-light ">
-          Real-time stock portfolio tracking with automatic updates every 15 seconds
-        </p>
-      </header>
+      <header className="bg-linear-to-r from-purple-900 via-purple-800 to-purple-900 shadow-xl border-b border-purple-700">
+  <div className="px-4 md:px-8 py-5 flex items-center justify-between">
+  <h1 className="text-3xl font-bold text-white tracking-tight flex items-center gap-2">
+  <span>Portfolio Dashboard</span>
+  <ChartBarIcon className="h-8 w-8 text-white" />
+</h1>
+    <div className="relative group">
+      <div className="w-11 h-11 bg-linear-to-br from-purple-600 to-purple-700 rounded-full flex items-center justify-center hover:from-purple-500 hover:to-purple-500 transition-all duration-200 cursor-pointer shadow-lg ring-2 ring-purple-500 hover:ring-purple-400">
+        <svg
+          className="w-6 h-6 text-white"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          />
+        </svg>
+      </div>
+      <div className="absolute right-0 top-full mt-2 px-3 py-1.5 bg-gray-800 text-white text-sm rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap shadow-lg">
+        Test User
+      </div>
+    </div>
+  </div>
+</header>
 
       <main>
         <PortfolioTable stocks={data} />
